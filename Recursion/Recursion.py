427552c369c39d57b1b8e1e22830b18fdcb67a4d
dnguyen0091 <davidnguyen0091@icloud.com>
2025-07-23 21:12:28 -0400

    feat: start of recursion notes

---
+++ Recursion/Recursion.py
@@ -0,0 +1,6 @@
+#**Recursion**
+
+#Recursion is a programming technique where a function calls itself to solve a problem. It is often used to break down complex problems into simpler subproblems.
+#Think of it like Russian dolls where each doll contains a smaller one til you hit the smallest doll.
+    #In recursion the base case would be the smallest doll, or the case where the function stops calling itself.
+    #In recursion you basically traverse til you hit the base case, then you start returning back up the stack of function calls.
